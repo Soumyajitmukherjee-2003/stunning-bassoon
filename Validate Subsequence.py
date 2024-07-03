e7b1e7417e79b13444bd973cb225ef2c2b16ab16
soumyajitM <soumyajitmukherjee2003@gmail.com>
2024-07-03 22:19:02 +0530

    corrections being updated and output of the code is added.

--- Validate Subsequence.py
+++ Validate Subsequence.py
@@ -1,9 +1,13 @@
 def isValidSubsequence(array,subsequence):
   i,j=0,0
-  while i<len(array) and j<len(sequence):
-    if array[i]==sequence[j]:
+  while i<len(array) and j<len(subsequence):
+    if array[i]==subsequence[j]:
       j+=1
     i+=1
   return j==len(subsequence)
   
   
+'''
+input:[1,2,3]
+Output:[1,2],[2,3]
+'''
