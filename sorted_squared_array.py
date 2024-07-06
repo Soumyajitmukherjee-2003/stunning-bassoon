a4fcc41022c39f4581d95c9c97c8eac47183fcab
soumyajitM <soumyajitmukherjee2003@gmail.com>
2024-07-06 11:21:06 +0530

    Input and Output is added

--- sorted_squared_array.py
+++ sorted_squared_array.py
@@ -1,5 +1,18 @@
+'''                             SORTED SQUARED ARRAY
+Write a function that takes in a non-empty array of integers that are sorted in ascending
+order and returns a new array of the same length with the squares of the original integers
+also sorted in ascending order.
+'''
+
+
+
 def sortedSquaredArray(array):
   squared_array=[num**2 for num in array]
   squared_array.sort()
   return squared_array
   
+''' SAMPLE INPUT
+array=[1,2,3,5,6,8,9]
+    SAMPLE OUTPUT
+array=[1,4,9,25,36,64,81]    
+'''
